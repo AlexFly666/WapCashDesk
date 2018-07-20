3a429313b61bf24e1e9cc634c2523bec88f583d2
AlexFly <fly910905@sina.com>
2018-07-20 10:22:28 +0800

    【180720】Wap收银台-初始化导入

---
+++ src/main/java/com/newcapec/wapcashdesk/WapCashDeskApplication.java
@@ -0,0 +1,12 @@
+package com.newcapec.wapcashdesk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WapCashDeskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WapCashDeskApplication.class, args);
+	}
+}
